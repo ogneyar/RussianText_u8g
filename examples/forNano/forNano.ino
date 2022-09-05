8c7283853de6bfddc76a09bab35f8a8dacba7c31
ogneyar <ogneyar-ne@ya.ru>
2022-09-05 20:15:55 +0300

    minor edit: edit example

--- examples/forNano/forNano.ino
+++ examples/forNano/forNano.ino
@@ -9,31 +9,30 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI
 #include <RussianText_u8g.h>
 
 
-void russian_text(void) {
-  
-  u8g.firstPage();
-  do {
-    u8g_prepare();
-    
-    // вывод русских букв на экран
-    send("абвгд", 0, 0);
-    send("абвгдеёж", 0, 10);
-    send("абвгдеёжзий", 0, 20); 
-
-    send("АБВГД", 0, 30);
-    send("АБВГДЕЁЖ", 0, 40);
-    send("АБВГДЕЁЖЗИЙ", 0, 50);
-
-    
-    u8g.drawLine(0, 0, 127, 0);
-    u8g.drawLine(0, 63, 127, 63);
-    
-  } while( u8g.nextPage() );
-  
+// вывод рамки на экран
+void border(void) {
+    u8g.drawLine(0, 0, 0, 63); // линия рамки слева 
+    u8g.drawLine(0, 0, 127, 0); // линия рамки сверху
+    u8g.drawLine(127, 0, 127, 63); // линия рамки справа
+    u8g.drawLine(0, 63, 127, 63); // линия рамки снизу
 }
 
+// вывод русских букв на экран
+void russian_text(void) {
+    send("абвгдеёжзийклм", 5, 15);
+    send("АБВГДЕЁЖЗИЙКЛМ", 5, 35);
+}
+
+
 void setup(void) {
-  russian_text();
+    u8g.firstPage();
+    do {
+        u8g_prepare();
+        
+        russian_text();
+        border();
+    
+    } while( u8g.nextPage() );
 }
 
 void loop(void) {
